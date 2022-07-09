519ab863da2017af927f21864947fef014cb2b18
Seto Korekiyo <jakus.superdev@gmail.com>
2022-07-09 18:38:58 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ const app = express();
 
 app.use("/", require("./routes/routes"));
 
-app.listen(5000, () => console.log("Server is running on 5000"));
+app.listen(4545, () => console.log("Server is running on 5000"));

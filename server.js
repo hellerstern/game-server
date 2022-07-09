a3077cd6c3cf4fc37ee4040916a4aee84c049e7a
Seto Korekiyo <jakus.superdev@gmail.com>
2022-07-09 19:10:11 +0900

    Updated port

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ const app = express();
 
 app.use("/", require("./routes/routes"));
 
-app.listen(5000, () => console.log("Server is running on 5000"));
+app.listen(process.env.PORT || 5000, () => console.log("Server is running on 5000"));

d865c3683afc11971e8f256cddec05e2cfbad43c
seto0125 <jakus.superdev@gmail.com>
2022-07-13 02:40:51 +0900

    cors error

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const axios = require("axios");
+const cors = require('cors');
 
 const app = express();
-
+app.use(cors({
+  origin: '*'
+}));
 app.use("/", require("./routes/routes"));
+
 
 app.listen(process.env.PORT || 5000, () => console.log("Server is running on 5000"));

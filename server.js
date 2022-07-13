88f241961b29f6157f6f88e95ca7a63866280e35
seto0125 <jakus.superdev@gmail.com>
2022-07-13 12:00:58 +0900

    remove router file

--- server.js
+++ server.js
@@ -4,26 +4,24 @@ const cors = require('cors');
 
 const app = express();
 
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 
-const allowlist = ['http://localhost:3000'];
+app.get('/test', (req, res) => {
+  (async () => {
+    const please = await axios.get(`https://m2-dev-controllermodz.aqeltech.com/rest/V1/products/byops5/options`, {
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8',
+        'Authorization': 'Bearer 7qcul86v2eqvursm9a60ecgk8g9ofz5m',
+        "Access-Control-Allow-Origin": '*',
+        "Access-Control-Allow-Credentials": true,
+        "Access-Control-Allow-Methods": "OPTIONS, GET, POST",
+        "Access-Control-Allow-Headers": "Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control"
+      }
+    });
+    res.json(please.data);
+  })();
+});
 
-    const corsOptionsDelegate = (req, callback) => {
-    let corsOptions;
-
-    let isDomainAllowed = whitelist.indexOf(req.header('Origin')) !== -1;
-    let isExtensionAllowed = req.path.endsWith('.jpg');
-    
-    if (isDomainAllowed && isExtensionAllowed) {
-      // Enable CORS for this request
-      corsOptions = { origin: true }
-    } else {
-      // Disable CORS for this request
-      corsOptions = { origin: false }
-    }
-    callback(null, corsOptions)
-  }
-
-app.use(cors(corsOptionsDelegate));
-  
-app.use("/", require("./routes/routes"));
 app.listen(process.env.PORT || 5000, () => console.log("Server is running on 5000"));

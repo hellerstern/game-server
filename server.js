8c6933a9f0ad9be40e8919aa13ce01d3252e6fb5
Webdragon0125 <jakus.superdev@gmail.com>
2022-07-23 00:07:46 +0900

    add product to cart with custom options

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const axios = require("axios");
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const { response } = require("express");
 
 const app = express();
 
 app.use(cors({
   origin: '*'
 }));
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+
 
 app.get('/test', (req, res) => {
   (async () => {
@@ -24,8 +31,7 @@ app.get('/test', (req, res) => {
   })();
 });
 
-app.get('/get_quote_id', (req, res) => {
-  (async () => {
+app.get('/get_quote_id', async (req, res) => {
     const please = await axios.post(`https://m2-dev-controllermodz.aqeltech.com/rest/V1/guest-carts`, {
       headers: {
         'Content-Type': 'application/json;charset=UTF-8',
@@ -36,7 +42,19 @@ app.get('/get_quote_id', (req, res) => {
       }
     });
     res.send(please.data.toString());
-  })();
-}) 
+});
+
+app.post('/add_product', async(req, res) => {
+  const please = await axios.post(`https://m2-dev-controllermodz.aqeltech.com/rest/V1/guest-carts/${req.body.cartItem.quoteId}/items`, req.body, {
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8',
+      "Access-Control-Allow-Origin": '*',
+      "Access-Control-Allow-Credentials": true,
+      "Access-Control-Allow-Methods": "OPTIONS, GET, POST",
+      "Access-Control-Allow-Headers": "Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control"
+    }
+  });
+  res.send(please.data);
+})
 
 app.listen(process.env.PORT || 5000, () => console.log("Server is running on 5000"));

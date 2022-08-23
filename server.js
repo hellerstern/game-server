c41e97ce132e014a18e9c45c60807a3464fd89ba
webdragon0125 <jakus.superdev@gmail.com>
2022-08-23 14:54:48 +0800

    add xbox version

--- server.js
+++ server.js
@@ -55,6 +55,23 @@ app.post('/add_product', async(req, res) => {
     }
   });
   res.send(please.data);
-})
+});
+
+
+app.get('/get_xbox_options', (req, res) => {
+  (async () => {
+    const please = await axios.get(`https://m2-dev-controllermodz.aqeltech.com/rest/V1/products/byoxbx/options`, {
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8',
+        'Authorization': 'Bearer 7qcul86v2eqvursm9a60ecgk8g9ofz5m',
+        "Access-Control-Allow-Origin": '*',
+        "Access-Control-Allow-Credentials": true,
+        "Access-Control-Allow-Methods": "OPTIONS, GET, POST",
+        "Access-Control-Allow-Headers": "Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control"
+      }
+    });
+    res.send(please.data);
+  })();
+});
 
 app.listen(process.env.PORT || 5000, () => console.log("Server is running on 5000"));

231bf8023cbe7e43e9ddeb75f5f49d92b7ad7492
Quynh NH <c0909h1179@gmail.com>
2022-09-06 02:29:10 +0700

    add API one for all

--- server.js
+++ server.js
@@ -15,6 +15,23 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
+app.get('/options', async (req, res) => {
+  if (!req.query || !req.query.sku) {
+    return res.status(400).send('SKU ID is required');
+  }
+  const please = await axios.get(`https://m2-dev-controllermodz.aqeltech.com/rest/V1/products/${req.query.sku}/options`, {
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8',
+      'Authorization': 'Bearer 7qcul86v2eqvursm9a60ecgk8g9ofz5m',
+      "Access-Control-Allow-Origin": '*',
+      "Access-Control-Allow-Credentials": true,
+      "Access-Control-Allow-Methods": "OPTIONS, GET, POST",
+      "Access-Control-Allow-Headers": "Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control"
+    }
+  });
+  res.send(please.data);
+});
+
 app.get('/test', (req, res) => {
   (async () => {
     // Hello.

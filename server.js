7eaf5a9e63a9999a741edfaffe275b43c823713f
Quynh NH <c0909h1179@gmail.com>
2022-09-06 23:58:12 +0700

    Update server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.get('/test', (req, res) => {
 });
 
 app.get('/get_quote_id', async (req, res) => {
-    const please = await axios.post(`https://m2-dev-controllermodz.aqeltech.com/rest/V1/guest-carts`, {
+    const createCart = await axios.post(`https://m2-dev-controllermodz.aqeltech.com/rest/V1/guest-carts`, {
       headers: {
         'Content-Type': 'application/json;charset=UTF-8',
         "Access-Control-Allow-Origin": '*',
@@ -62,7 +62,27 @@ app.get('/get_quote_id', async (req, res) => {
         "Access-Control-Allow-Headers": "Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control"
       }
     });
-    res.send(please.data.toString());
+    if (createCart.data.toString() !== '')
+    {
+      const getQuoteId = await axios.get(`https://m2-dev-controllermodz.aqeltech.com/rest/V1/guest-carts/${createCart.data.toString()}`, {
+        headers: {
+          'Content-Type': 'application/json;charset=UTF-8',
+          'Authorization': 'Bearer 7qcul86v2eqvursm9a60ecgk8g9ofz5m',
+          "Access-Control-Allow-Origin": '*',
+          "Access-Control-Allow-Credentials": true,
+          "Access-Control-Allow-Methods": "OPTIONS, GET, POST",
+          "Access-Control-Allow-Headers": "Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control"
+        }
+      });
+      res.send(getQuoteId.data);
+    }
+    else
+    {
+      res.send({
+        success: false,
+        message: 'Wrong when creating Cart'
+      });
+    }
 });
 
 app.post('/add_product', async(req, res) => {

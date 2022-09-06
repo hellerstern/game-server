2518703d765009382edfd393b2d8cdaf0c4af647
Quynh NH <c0909h1179@gmail.com>
2022-09-07 00:07:38 +0700

    Update server.js

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.get('/get_quote_id', async (req, res) => {
           "Access-Control-Allow-Headers": "Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control"
         }
       });
-      res.send({cartId: createCart.data.toString(), quoteData: getQuoteId.data});
+      res.send({quote_id: createCart.data.toString(), cartData: getQuoteId.data});
     }
     else
     {

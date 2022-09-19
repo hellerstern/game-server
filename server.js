ddd682f7df82213fd433dafb8e02f6d67ba6010c
Quynh NH <c0909h1179@gmail.com>
2022-09-19 17:30:46 +0700

    Update server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.get('/options', async (req, res) => {
   if (!req.query || !req.query.sku) {
     return res.status(400).send('SKU ID is required');
   }
-  const please = await axios.get(`https://dev.controllermodz.co.uk/rest/V1/products/${req.query.sku}/options`, {
+  const please = await axios.get(`https://m2-dev-controllermodz.aqeltech.com/rest/V1/products/${req.query.sku}/options`, {
     headers: {
       'Content-Type': 'application/json;charset=UTF-8',
       'Authorization': 'Bearer 7qcul86v2eqvursm9a60ecgk8g9ofz5m',
@@ -75,7 +75,7 @@ app.get('/get_estimated_delivery', async (req, res) => {
   if (!req.query || !req.query.product_id) {
     return res.status(400).send('SKU ID is required');
   }
-  const please = await axios.get(`https://dev.controllermodz.co.uk/estimateddeliverydate/message/update/id/${req.query.product_id}`, {
+  const please = await axios.get(`https://m2-dev-controllermodz.aqeltech.com/estimateddeliverydate/message/update/id/${req.query.product_id}`, {
     headers: {
       'Content-Type': 'application/json;charset=UTF-8',
       'Authorization': 'Bearer 7qcul86v2eqvursm9a60ecgk8g9ofz5m',
@@ -91,7 +91,7 @@ app.get('/get_estimated_delivery', async (req, res) => {
 app.get('/test', (req, res) => {
   (async () => {
     // Hello.
-    const please = await axios.get(`https://dev.controllermodz.co.uk/rest/V1/products/byops5/options`, {
+    const please = await axios.get(`https://m2-dev-controllermodz.aqeltech.com/rest/V1/products/byops5/options`, {
       headers: {
         'Content-Type': 'application/json;charset=UTF-8',
         'Authorization': 'Bearer 7qcul86v2eqvursm9a60ecgk8g9ofz5m',
@@ -109,7 +109,7 @@ app.get('/get_product', async (req, res) => {
   if (!req.query || !req.query.quoteId) {
     return res.status(400).send('quoteId is required');
   }
-  const please = await axios.get(`https://dev.controllermodz.co.uk/rest/V1/guest-carts/${req.query.quoteId}/items`, {
+  const please = await axios.get(`https://m2-dev-controllermodz.aqeltech.com/rest/V1/guest-carts/${req.query.quoteId}/items`, {
     headers: {
       'Content-Type': 'application/json;charset=UTF-8',
       'Authorization': 'Bearer 7qcul86v2eqvursm9a60ecgk8g9ofz5m',
@@ -123,7 +123,7 @@ app.get('/get_product', async (req, res) => {
 });
 
 app.get('/get_quote_id', async (req, res) => {
-  const createCart = await axios.post(`https://dev.controllermodz.co.uk/rest/V1/guest-carts`, {
+  const createCart = await axios.post(`https://m2-dev-controllermodz.aqeltech.com/rest/V1/guest-carts`, {
     headers: {
       'Content-Type': 'application/json;charset=UTF-8',
       "Access-Control-Allow-Origin": '*',
@@ -135,7 +135,7 @@ app.get('/get_quote_id', async (req, res) => {
   res.send(createCart.data.toString());
   // if (createCart.data.toString() !== '')
   // {
-  //   const getQuoteId = await axios.get(`https://dev.controllermodz.co.uk/rest/V1/guest-carts/${createCart.data.toString()}`, {
+  //   const getQuoteId = await axios.get(`https://m2-dev-controllermodz.aqeltech.com/rest/V1/guest-carts/${createCart.data.toString()}`, {
   //     headers: {
   //       'Content-Type': 'application/json;charset=UTF-8',
   //       'Authorization': 'Bearer 7qcul86v2eqvursm9a60ecgk8g9ofz5m',
@@ -158,7 +158,7 @@ app.get('/get_quote_id', async (req, res) => {
 app.post('/update_product', async(req, res) => {
   try
   {
-    const please = await axios.put(`https://dev.controllermodz.co.uk/rest/V1/guest-carts/${req.body.cartItem.quoteId}/items/${req.body.itemId}`, {
+    const please = await axios.put(`https://m2-dev-controllermodz.aqeltech.com/rest/V1/guest-carts/${req.body.cartItem.quoteId}/items/${req.body.itemId}`, {
       cartItem: req.body.cartItem
     }, {
       headers: {
@@ -183,7 +183,7 @@ app.post('/update_product', async(req, res) => {
 app.post('/add_product', async(req, res) => {
   try
   {
-    const please = await axios.post(`https://dev.controllermodz.co.uk/rest/V1/guest-carts/${req.body.cartItem.quoteId}/items`, {
+    const please = await axios.post(`https://m2-dev-controllermodz.aqeltech.com/rest/V1/guest-carts/${req.body.cartItem.quoteId}/items`, {
       cartItem: req.body.cartItem
     }, {
       headers: {
@@ -208,7 +208,7 @@ app.post('/add_product', async(req, res) => {
 
 app.get('/get_xbox_options', (req, res) => {
   (async () => {
-    const please = await axios.get(`https://dev.controllermodz.co.uk/rest/V1/products/byoxbx/options`, {
+    const please = await axios.get(`https://m2-dev-controllermodz.aqeltech.com/rest/V1/products/byoxbx/options`, {
       headers: {
         'Content-Type': 'application/json;charset=UTF-8',
         'Authorization': 'Bearer 7qcul86v2eqvursm9a60ecgk8g9ofz5m',
